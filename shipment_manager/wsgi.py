bb065bdc3bd0989668bffddcb18feffbb1c01098
S-telleManon <manonstelle888@gmail.com>
2026-05-19 08:50:14 +0400

    test2

--- shipment_manager/wsgi.py
+++ shipment_manager/wsgi.py
@@ -7,10 +7,28 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shipment_manager.settings')
+
+# application = get_wsgi_application()
+
+import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Force project directories into Python's search path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_dir)
+
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shipment_manager.settings')
 
 application = get_wsgi_application()
+app = application
