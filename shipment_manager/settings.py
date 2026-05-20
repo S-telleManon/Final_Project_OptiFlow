41d03251616c64fffff88d9fa4a5ad04be870b9d
S-telleManon <manonstelle888@gmail.com>
2026-05-20 13:29:44 +0400

    Revert "trystatic"
    This reverts commit a9757af81f05d7fcfc9a7ca2a52756179d03b829.

--- shipment_manager/settings.py
+++ shipment_manager/settings.py
@@ -46,13 +46,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -131,9 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = []
 
 # This is the exact destination Vercel expects to find
-# STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 
 LOGIN_URL = '/shipments/login/'  
 LOGIN_REDIRECT_URL = '/shipments/list/'  
